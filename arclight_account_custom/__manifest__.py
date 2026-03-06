8d9615d5637b4b65ea22f12cbf9f2e976ad490f4
Bhavik <bhavik@Bhaviks-MacBook-Air.local>
2026-03-06 15:36:37 +0530

    [MOD]resolve conflict

--- arclight_account_custom/__manifest__.py
+++ arclight_account_custom/__manifest__.py
@@ -7,24 +7,18 @@
         'dynamic_accounts_report',
         'texbyte_gstr',
         'stock',
-<<<<<<< Updated upstream
         'sale_management',
         'sale_stock',
-=======
         'ecartes_report',
->>>>>>> Stashed changes
     ],
     'data': [
         'security/ir.model.access.csv',
         'views/item_ledger_wizard_views.xml',
         'report/item_ledger_report.xml',
-<<<<<<< Updated upstream
         'views/pending_dc_report_wizard_views.xml',
-=======
         'report/tax_invoice_template.xml',
         'report/invoice_slip_inherit.xml',
         'report/account_tax_invoice_report_action.xml',
->>>>>>> Stashed changes
         'views/dynamic_accounts_menu.xml',
         'views/hsn_summary_views.xml',
         'views/stock_move_line_views.xml',
